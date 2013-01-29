0ee0abfa2c4fcd9d0c45756d19b882c14617ab33
Alexei Pozdnyakov <alexeipozdnyakov@MacBook-Pro-Alexei.local>
2013-01-29 21:37:29 +0700

    Initial Commit

---
+++ GYMaaS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GYMaaS
+//
+//  Created by Alexei Pozdnyakov on 29.01.13.
+//  Copyright (c) 2013 Alexei Pozdnyakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
